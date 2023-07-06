3d64631445c527538f559f4e835a19acad154354
Dan-B12 <dan_beech@live.co.uk>
2023-07-06 20:03:44 +0100

    Fetch was not inside getCityData function

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 const city = 'city-input'
 const apiKey = 'a2e63e109ef5e39ec0caed175e3283bfae3633dc72c982857186997a3cbe03a4';
 
-function getCityData(city) {
-const options = { method: 'GET', headers: { accept: "application/json"}
-  }
-};
+function getCityData(city, apiKey) {
+const options = { 
+  method: 'GET', 
+  headers: { 
+    accept: "application/json",
+    'Authorization': `Bearer ${apiKey}`
+    }
+  };
 
-fetch(`https://api.openaq.org/v2/measurements?city=$(city)`, options) 
-  .then(response => response.json())
-  .then(data => {
-    // Iterate over the results from the API
-    data.results.forEach(result => {
-      // For each result, create a new p element and set its text to the parameter and value
-      result.measurements.forEach(measurement => {
-          var para = document.createElement('p');
-          para.textContent = `${measurement.parameter}: ${measurement.value} ${measurement.unit}`;
-          displayDiv.appendChild(para);
-      });
-  });
-})
-.catch(err => console.error(err));
-
+  fetch(`https://api.openaq.org/v2/measurements?city=$(city)`, options) 
+    .then(response => response.json())
+    .then(data => {
+      // Iterate over the results from the API
+      data.results.forEach(result => {
+        // For each result, create a new p element and set its text to the parameter and value
+        result.measurements.forEach(measurement => {
+            var para = document.createElement('p');
+            para.textContent = `${measurement.parameter}: ${measurement.value} ${measurement.unit}`;
+            displayDiv.appendChild(para);
+        });
+    });
+  })
+  .catch(err => console.error(err));
+}
 document.addEventListener("DOMContentLoaded", function() {
   var city = sessionStorage.getItem('city');
   var displayDiv = document.getElementById('city-data');
